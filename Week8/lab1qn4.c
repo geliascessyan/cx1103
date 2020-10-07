f8dc1e1630f06238f5fc498bc17d04cdf660989a
geliascessyan <70942834+geliascessyan@users.noreply.github.com>
2020-10-07 16:06:37 +0800

    Update lab1qn4.c
    Changed power function

--- Week8/lab1qn4.c
+++ Week8/lab1qn4.c
@@ -6,8 +6,8 @@ int main() {
     scanf("%lf", &x);
 
     for (int n=1; n<=30; n++) {
-        double numerator = x, denominator = 1.0;
-        for (int power=1; power<n; power++) {
+        double numerator = 1.0, denominator = 1.0;
+        for (int power=1; power<=n; power++) {
             numerator *= x;
         }
         for (int p=1; p<=n; p++) {

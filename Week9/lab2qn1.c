1cc9699079810eadaeb79dcfedd82db4fa40cdb8
geliascessyan <70942834+geliascessyan@users.noreply.github.com>
2020-10-14 13:53:35 +0800

    Add files via upload

--- Week9/lab2qn1.c
+++ Week9/lab2qn1.c
@@ -21,7 +21,22 @@ int numDigits1(int num) {
    } while (num > 0);    
    return count;
 }
+
+void numDigits2(int num, int *result) {
+      *result = 0;
+      //printf("test result %d\n", *result);
+   do {
+      //*result++;
+      (*result)++;
+      //printf("test result %d\n", *result);
+      num = num/10; // (int) num/10
+   } while (num > 0);    
+   //*result = count;   
+}
+
+/*
 void numDigits2(int num, int *result)
 {
    *result = numDigits1(num);
 }
+*/

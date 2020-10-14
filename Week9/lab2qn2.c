d3a9ef845d13da24125f39c5feb39154bdf29602
geliascessyan <70942834+geliascessyan@users.noreply.github.com>
2020-10-14 13:56:44 +0800

    Add files via upload

--- Week9/lab2qn2.c
+++ Week9/lab2qn2.c
@@ -7,8 +7,10 @@ int main() {
    printf("Enter the number: \n");
    scanf("%d", &number);
    printf("Enter the digit: \n");
-   scanf("%d", &digit);             
-   printf("digitPos1(): %d\n", digitPos1(number, digit));            
+   scanf("%d", &digit);         
+
+   printf("digitPos1(): %d\n", digitPos1(number, digit));    
+
    digitPos2(number, digit, &result);           
    printf("digitPos2(): %d\n", result);    
    return 0;
@@ -18,7 +20,7 @@ int digitPos1(int num, int digit) {
     int pos=0;
     do {
        pos++;
-       printf("test %d\n",pos);
+       //printf("test %d\n",pos);
        if (num%10 == digit){
            return pos;
        }
@@ -34,7 +36,14 @@ void digitPos2(int num, int digit, int *result) {
        pos++;
        if (num%10 == digit) {
          *result = pos;
+         break;
        }
        num = num/10;
     } while (num > 0);    
 }
+
+/*
+void digitPos2(int num, int digit, int *result) {
+    *result = digitPos1(num, digit);
+}
+*/
